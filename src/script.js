a6a4f8dd367d1940b482a71ce7ec1cacdf3ad0df
Mumina033 <mumina.mohamed033@gmail.com>
2024-02-05 20:14:30 -0500

    rewrote the context and made changes to the css

--- src/script.js
+++ src/script.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
   let apiKey = "f9e42a1811b99caa0995a3tbf6o8c341";
   let prompt = `User instructions: Write a short heartfelt love poem about ${instructionsInput.value}`;
   let context =
-    "As a versatile poet, craft brief and heartwarming expressions of love. Explore themes such as friendship, lost connections, parental affection, universal love, LGBTQ+ bonds, self-love, and eternal connections. Ensure each poem is concise, avoiding repetitive use of the word 'love.' Aim to encapsulate the essence of love in 4 lines presented in basic HTML. Do not add css or change the styling of the poem. Please follow the provided user instructions for guidance.";
+    "As a versatile poet, craft brief and heartwarming expressions of love. Explore themes such as friendship, lost connections, parental affection, universal love, LGBTQ+ bonds, self-love, and eternal connections. Ensure each poem is concise, avoiding use of the word 'love.' Aim to encapsulate the essence of love in 4 lines presented in basic HTML and seperate each line with a <br />. Do not add css or change the styling of the poem. Please follow the provided user instructions for guidance.";
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
